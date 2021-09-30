be09a24ae790f91d25fe428131084753d0092e65
kasra <kasramohamadpor@yahoo.com>
2021-09-30 15:44:36 +0330

    validate sign up page

--- src/SignUp/SignUp.js
+++ src/SignUp/SignUp.js
@@ -1,133 +1,168 @@
-import React from "react";
-import { useState } from "react";
+import React, { useRef } from "react";
+import { useForm } from "react-hook-form";
 import { Link } from "react-router-dom";
 
 const SignUp = () => {
-  const [state, setState] = useState({
-    fname: "",
-    lname: "",
-    email: "",
-    password: "",
-    check: false,
-    data: [],
-  });
-
-  const handleInputChange = (e) => {
-    setState((prevState) => ({
-      ...prevState,
-      [e.target.name]: e.target.value,
-    }));
-  };
-  const handleSubmit = (e) => {
-    setState((prevState) => ({
-      data: [
-        ...prevState.data,
-        {
-          fname: prevState.fname,
-          lname: prevState.lname,
-          email: prevState.email,
-          password: prevState.password,
-          check: prevState.check,
-        },
-      ],
-      fname: "",
-      lname: "",
-      email: "",
-      password: "",
-      check: false,
-    }));
-    e.preventDefault();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    reset,
+    trigger,
+    watch,
+    setValue,
+  } = useForm();
+  const password = useRef();
+  password.current = watch("password");
+  const onsubmit = (data) => {
+    console.log(data);
+    reset();
   };
   return (
     <div className="d-flex justify-content-center mt-3">
       <div className="card w-75 shadow">
         <div className="card-body ">
           <h3 className="card-title text-center mb-5">Sign Up</h3>
-          <form className="row needs-validation" onSubmit={handleSubmit}>
+          <form className="row" onSubmit={handleSubmit(onsubmit)}>
             <div className="col-md-6 mb-3 form-floating">
               <input
                 type="text"
-                name="fname"
-                value={state.fname}
-                onChange={handleInputChange}
                 className="form-control"
+                {...register("fname", { required: "Frist Name Is Required" })}
+                onChange={(e) => setValue("fname", e.target.value.trim())}
                 placeholder="Frist Name"
                 id="fname"
-                required
               />
-              <label for="fname" className="ms-2 text-muted">
+              <label htmlFor="fname" className="ms-2 text-muted">
                 Frist name
               </label>
+              {errors.fname && (
+                <small className="text-danger">{errors.fname.message}</small>
+              )}
             </div>
             <div className="col-md-6 mb-3 form-floating">
               <input
                 type="text"
-                name="lname"
-                value={state.lname}
-                onChange={handleInputChange}
                 className="form-control"
+                {...register("lname", { required: "Last Name Is Required" })}
+                onChange={(e) => setValue("lname", e.target.value.trim())}
                 placeholder="Last Name"
                 id="lname"
-                required
               />
-              <label for="lname" className="ms-2 text-muted">
+              <label htmlFor="lname" className="ms-2 text-muted">
                 Last name
               </label>
+              {errors.lname && (
+                <small className="text-danger">{errors.lname.message}</small>
+              )}
             </div>
 
             <div className="col-12 mb-3 form-floating">
               <input
                 type="email"
-                name="email"
-                value={state.email}
-                onChange={handleInputChange}
                 className="form-control"
+                {...register("email", {
+                  required: "Email Is Required",
+                  pattern: {
+                    value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                    message: "Invalid email address",
+                  },
+                })}
+                onKeyUp={() => {
+                  trigger("email");
+                }}
+                onChange={(e)=>setValue("email",e.target.value.trim())}
                 placeholder="name@example.com"
                 id="email"
-                required
               />
-              <label for="email" className="ms-2 text-muted">
+              <label htmlFor="email" className="ms-2 text-muted">
                 Email
               </label>
+              {errors.email && (
+                <small className="text-danger">{errors.email.message}</small>
+              )}
             </div>
             <div className="col-12 mb-3 form-floating">
               <input
                 type="password"
-                name="password"
-                value={state.password}
-                onChange={handleInputChange}
                 className="form-control"
+                {...register("password", {
+                  required: "Password Is Required",
+                  // pattern: {
+                  //   value:
+                  //     /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*_=+-]).{8,12}$/,
+                  //   message: "Passowrd Is Invalid",
+                  // },
+                  minLength: {
+                    value: 8,
+                    message: "password must be 8 charcter or more",
+                  },
+                })}
+                onKeyUp={() => {
+                  trigger("password");
+                }}
+                onChange={(e) => setValue("password", e.target.value.trim())}
                 placeholder="Password"
-                minLength="8"
                 id="password"
-                required
               />
-              <label for="password" className="ms-2 text-muted">
+              <label htmlFor="password" className="ms-2 text-muted">
                 Password
               </label>
+              {errors.password && (
+                <small className="text-danger">{errors.password.message}</small>
+              )}
+            </div>
+            <div className="col-12 mb-3 form-floating">
+              <input
+                type="password"
+                className="form-control"
+                {...register("passwordConfrim", {
+                  required: true,
+                  validate: (value) => value === password.current,
+                })}
+                onKeyUp={() => {
+                  trigger("passwordConfrim");
+                }}
+                onChange={(e) =>
+                  setValue("passwordConfrim", e.target.value.trim())
+                }
+                placeholder="Password"
+                id="passwordConfrim"
+              />
+              <label htmlFor="passwordConfrim" className="ms-2 text-muted">
+                Password Confrim
+              </label>
+              {errors.passwordConfrim &&
+                errors.passwordConfrim.type === "required" && (
+                  <small className="text-danger">
+                    Password Confrim Is Required
+                  </small>
+                )}
+              {errors.passwordConfrim &&
+                errors.passwordConfrim.type === "validate" && (
+                  <small className="text-danger">Password do not match</small>
+                )}
             </div>
             <div className="col-12 mb-3">
               <div className="form-check">
                 <input
                   type="checkbox"
-                  name="check"
-                  value={state.check}
-                  onChange={handleInputChange}
                   className="form-check-input"
+                  {...register("checked", {
+                    required: "You must agree before submitting",
+                  })}
                   id="invalidCheck"
-                  checked={state.check}
-                  required
                 />
                 <label
                   className="form-check-label text-start"
-                  for="invalidCheck"
+                  htmlFor="invalidCheck"
                 >
                   Agree to <Link to="#">terms and conditions</Link>
                 </label>
-                <div className="invalid-feedback">
-                  You must agree before submitting.
-                </div>
               </div>
+              {errors.checked && (
+                <small className="text-danger">{errors.checked.message}</small>
+              )}
             </div>
             <div className="col-12 mb-3">
               <button className="btn btn-primary" type="submit">
